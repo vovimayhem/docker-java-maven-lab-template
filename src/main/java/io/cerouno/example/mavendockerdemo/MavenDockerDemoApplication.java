a0ad92081b0cdf4dae71abb6ba6055294bc7f497
Roberto Quintanilla <roberto.quintanilla@gmail.com>
2020-09-01 14:47:13 -0500

    Initial commit

---
+++ src/main/java/io/cerouno/example/mavendockerdemo/MavenDockerDemoApplication.java
@@ -0,0 +1,13 @@
+package io.cerouno.example.mavendockerdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MavenDockerDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MavenDockerDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/cerouno/example/mavendockerdemo/MavenDockerDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package io.cerouno.example.mavendockerdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MavenDockerDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
